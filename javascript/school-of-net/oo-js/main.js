8cd3f2de6110608f6d112c2bf23a09a35fd78447
Adriano Ohana <adriano.ohana@totvs.com.br>
2017-10-20 19:09:37 -0200

    including java OOP

--- javascript/school-of-net/oo-js/main.js
+++ javascript/school-of-net/oo-js/main.js
@@ -1,22 +1,52 @@
-class Person {
-    constructor() {
-        this.name;
-        this.age;
-        this.height;
-        this.capacity;
+function Person(_name, _age, _height) {
+    
+    var name   = _name;
+    var age    = _age; 
+    var height = _height;
+
+    function privateFunc() {
+        console.log("Won't be accessible externaly");
     }
 
-    sayHello() {
-        for (var i = 0; i < this.capacity; i++) {
+    this.sayHello = function () {
+        for (var i = 0; i < 5; i++) {
             console.log('Hello ' + i);
         }
     }
+
+    this.setName = function (_name) {
+        name = _name;
+    }
+
+    this.setAge = function (_age) {
+        age = _age;
+    }
 }
 
-var adriano = new Person();
-adriano.name = 'Adriano Martins Ohana';
-adriano.age = 34;
-adriano.height = 179;
-adriano.capacity = 10;
+Person.prototype.myProtoFunc = function () {
+    console.log('First prototype function');
+}
 
-adriano.sayHello();
+var adriano = new Person('Adriano Ohana', 34, 179);
+
+Person.say_hello = function () {
+    for (var i = 0; i < 5; i++) {
+        console.log('Static Hello ' + i);
+    }
+}
+
+adriano.myProtoFunc();
+
+//Person.say_hello();
+
+console.log(adriano);
+
+// Testing prototype object
+console.log(adriano.__proto__);
+console.log(Person.prototype);
+
+//adriano.sayHello();
+
+
+// Testing private functions in JS
+//adriano.privateFunc();
