25617773bc99336e34c3221b65a8377427d9313a
Adriano Ohana <adriano.ohana@totvs.com.br>
2017-11-05 19:29:48 -0200

    continuing studies

---
+++ javascript/school-of-net/es6-project/README.md
@@ -0,0 +1,9 @@
+Using Babel Cli and Preset-ES2015.
+
+Command to install packages in dev enviroment:
+
+npm install babel-cli babel-preset-es2015 --save-dev
+
+Command to execute:
+
+npm run babel
--- javascript/school-of-net/es6-project/js/main.js
+++ javascript/school-of-net/es6-project/js/main.js
@@ -1,10 +1,85 @@
-"use strict";
+'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+// ############# First example ############## /
+/**
+var bob = {
+    _name: "Bob",
+    _friends:['Carol', 'Steve', 'Roger'],
+    
+    printFriends : function printFriends() {
+        var self = this;
+        this._friends.forEach(function(f){
+            console.log(self._name + " knows " + f);
+        });
+    }
+}
 
-var Client = function Client(name, email) {
-    _classCallCheck(this, Client);
+bob.printFriends();
+*/
 
-    this.name = name;
-    this.email = email;
-};
+// ############# Arrow concepts ############## /
+/**
+ var color = () => console.log("Red");
+color();
+
+
+var bob = {
+    _name: "Bob",
+    _friends:['Carol', 'Steve', 'Roger'],
+    
+    printFriends : function printFriends() {
+        this._friends.forEach(f => 
+            console.log(this._name + " knows " + f));
+    }
+}
+
+bob.printFriends();
+*/
+
+// ############# Using class ############## /
+/**class Color {
+    constructor(code, name){
+        this.code = code;
+        this.name = name;
+    };
+
+    getColor() {
+        return {code: this.code, name: this.name};
+    };
+}
+
+let red = new Color('red', 'Red');
+console.log(red.getColor());*/
+
+// ############# String template ############## /
+/**let name = "Guilherme";
+let age = 19;
+//let text = "name: " + name + " age: " + age;
+let text = `
+<p>Name: ${name}</p>
+<p>Age: ${age}</p>
+`;*/
+
+// ############# Default values ############## /
+// Old way
+// function test(valueA) {
+//     var valueB = arguments.length <= 1 || arguments[1] === "undefined" ? 4 : arguments[1];
+//     return valueA + valueB;
+// }
+
+// console.log(test(2,5));
+
+// Using DV
+// function test(valueA, valueB = 4) {
+//     return valueA + valueB;
+// }
+
+// console.log(test(2));
+
+// ############# For OF ############## /
+
+var list = ['Adriano', 34, { color: 'red' }];
+
+for (var i in list) {
+    console.log(list[i]);
+}
