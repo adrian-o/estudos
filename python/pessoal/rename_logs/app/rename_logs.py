97cec896d4caad24fa13b002bbe5ab4b9d2fcfae
Adriano Ohana <adriano.ohana@totvs.com.br>
2018-10-14 16:34:27 -0300

    Ajustando rename de logs.

--- python/pessoal/rename_logs/app/rename_logs.py
+++ python/pessoal/rename_logs/app/rename_logs.py
@@ -1,9 +1,12 @@
 import os
 import os.path
 import shutil
+from datetime import date
+
 
 main_dir = 'C:\log'
 list_file_logs = []
+
 
 def is_file_log(my_dir):
     last_part = my_dir.split('.')[-1]
@@ -18,8 +21,8 @@ def list_all_logs(path_dir):
         verify_dir = os.path.join(path_dir, my_dir)
         
         if os.path.isdir(verify_dir):
-            print('Dir: ' + my_dir)
-            print('Listing all file logs...')
+            print('dir: ' + my_dir)
+            print('listing all file logs...')
             list_all_logs(os.path.join(path_dir, my_dir))
         else:
             if is_file_log(my_dir):
@@ -27,16 +30,23 @@ def list_all_logs(path_dir):
 
 
 
-def rename_log_file():
+def rename_log_file(log_file):
+    file_parts = log_file.split('.')
+    log_file_new = log_file + '.' + str(date.today())
+    
+    os.rename(log_file, log_file_new)
      
+
 
 def main():
     if not os.path.exists(main_dir):
-        print('Erro do caraleo...')
+        print('file path does\'nt exists...')
     else:
         list_all_logs(main_dir)
 
-        for file_logs in list_file_logs:
-            print('log file: ' + file_logs)
+        for log_file in list_file_logs:
+            print('log file: ' + log_file)
+            print('renaming...')
+            rename_log_file(log_file)
 
 main()
