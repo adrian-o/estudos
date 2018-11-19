df36b3787e403e000cf8663891cc5f098ce53b96
adriano.ohana <adriano.ohana@gmail.com>
2018-11-19 01:12:18 -0200

    estudos springboot

---
+++ java/spring/spring-boot-microservices-book/green-dog-app/src/test/java/com/github/adriano/estudos/java/spring/GreenDogAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.adriano.estudos.java.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreenDogAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
